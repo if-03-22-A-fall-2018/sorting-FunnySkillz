0b5c6ae57e2b538b2c7f154cc609f2a4a57efc65
FunnySkillz <sebastijan.bogdan@outlook.com>
2019-03-05 11:01:35 +0100

    finished

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -10,3 +10,45 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "stopwatch.h"
+#include "general.h"
+#include "sorting_algorithms.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void 	init_random (int *array, unsigned long length){
+  for (int i = 1; i <= length; i++) {
+    array[i] = rand() % 100 + 1;
+  }
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void 	bubble_sort (int *array, unsigned long length){
+  int i, j;
+  for (i = 0; i < length-1; i++)
+      for (j = 0; j < length-i-1; j++)
+          if (array[j] > array[j+1])
+             swap(&array[j], &array[j+1]);
+}
+
+void 	insertion_sort (int *array, unsigned long length){
+   int key, j;
+   for (int i = 1; i < length; i++)
+   {
+       key = array[i];
+       j = i-1;
+
+       while (j >= 0 && array[j] > key)
+       {
+           array[j+1] = array[j];
+           j = j-1;
+       }
+       array[j+1] = key;
+   }
+}
--- stopwatch.cpp
+++ stopwatch.cpp
@@ -10,3 +10,21 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "stopwatch.h"
+#include "general.h"
+#include "sorting_algorithms.h"
+#include "time.h"
+
+clock_t start, end;
+double cpu_time_used;
+
+void 	start_stopwatch (){
+start = clock();
+}
+void 	stop_stopwatch (){
+
+}
+float elapsed_time (){
+end = clock();
+ return cpu_time_used = ((float) (end - start)) /CLOCKS_PER_SEC;
+}
